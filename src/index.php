0709b8867a071eed037fa842d65e90ea7773be73
Emrehan Yurdakul <149236187+YEmrehan@users.noreply.github.com>
2025-06-03 18:03:08 +0300

    updated

--- src/index.php
+++ src/index.php
@@ -1,11 +1,12 @@
 <?php
 
-require_once __DIR__ . '/vendor/autoload.php';
+require_once __DIR__ . '/../vendor/autoload.php';
 
 use Dotenv\Dotenv;
 
 $dotenv = Dotenv::createImmutable(__DIR__);
 $dotenv->load();
+
 
 // Desteklenen diller
 $supportedLanguages = ['tr', 'en'];
